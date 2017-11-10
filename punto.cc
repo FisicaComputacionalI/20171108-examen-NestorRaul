5d5712bc91ac03e357e4791b529a90d001b01a43
NestorRaul <31254585+NestorRaul@users.noreply.github.com>
2017-11-10 13:51:08 -0600

    Add files via upload

---
+++ punto.cc
@@ -0,0 +1,16 @@
+//Programa de punto extra
+//Nestor Raul Mancilla Xinto
+#include<iostream>
+using namespace std;
+
+int main(){
+ double D,T,V;
+
+ cout<<"Dame una distancia en metros,solo numero"<<endl;
+ cin>>D;
+ cout<<"Dame un tiempo en segundos, solo numero"<<endl;
+ cin>>T;
+ V=D/T;
+ cout<<"La velocidad es "<<V<<"  m/s"<<endl;
+ return 0;
+}
